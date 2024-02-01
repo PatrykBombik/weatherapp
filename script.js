8b6e7a256347721e1de84ba3d223b4740aa08f0a
Patryk Bombik <patryk.bombik@gmail.com>
2024-02-01 20:53:04 +0100

    weather api done

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    let data = await response.json();
 
-    console.log(data);
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
+
+    }
+
+    let data = await response.json();
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
@@ -31,6 +37,10 @@ async function checkWeather(city) {
     else if(data.weather[0].main == "Mist") {
         weatherIcon.src = "images/mist.png"
     }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+    
 }
 
 searchBtn.addEventListener("click", ()=>{
